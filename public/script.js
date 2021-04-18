cd50e6ef6d70a4990a0b7396bb0d7fa56de9094f
Cores13 <64189850+Cores13@users.noreply.github.com>
2021-04-19 00:27:58 +0200

    css

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@
 function changeTodo(req, url){
     //get id of a task
     var txt = req.textContent;
+    console.log(txt);
     var fullId =  req.id;
     fullId = fullId.substring(fullId.indexOf(":")+1);
     //remove label and other elements
@@ -14,10 +15,11 @@ function changeTodo(req, url){
     form.setAttribute('id', `${fullId}`);
     form.setAttribute('action', `${url}/change-todo/${fullId}`);
     form.setAttribute('method', 'POST');
+    form.setAttribute('class', 'edit-form');
     //create edit and label elements and give them attributes
     var edit = document.createElement('input');
     edit.setAttribute('id', 'edit');
-    edit.setAttribute('id', 'edit');
+    edit.setAttribute('class', 'edit');
     edit.setAttribute('type', 'text');
     edit.setAttribute('name', 'todo');
     edit.setAttribute('value',txt);
@@ -29,4 +31,5 @@ function changeTodo(req, url){
     //append edit to form ellement
     form.appendChild(edit);
     document.getElementById(`f:${fullId}`).appendChild(form);
+    document.getElementById(`f:${fullId}`).setAttribute('style', 'padding-top: 0; padding-bottom: 0;')
 }
